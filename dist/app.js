a27a7df71e2452096489a42a9eb0b52fe1135d92
S.Panfilov <se-panfilov@ya.ru>
2015-09-04 17:01:03 +0800

    fixes for file reading

--- dist/app.js
+++ dist/app.js
@@ -25,33 +25,6 @@ angular.module('outstanding', [
 
 'use strict';
 
-angular.module('outstanding.pages.landing', [
-    'outstanding.calendar',
-    'outstanding.date_details',
-    'outstanding.uploader',
-    'ui.router'
-])
-
-    .config(['$stateProvider', function ($stateProvider) {
-
-        $stateProvider
-            .state('landing', {
-                url: '/landing',
-                templateUrl: 'landing/landing.html',
-                controller: 'LandingPageCtrl'
-            })
-        ;
-    }])
-
-    .controller('LandingPageCtrl', ['$scope', function ($scope) {
-
-        console.log('landing');
-        
-    }])
-;
-
-'use strict';
-
 angular.module('outstanding.calendar', [])
 
     .directive('calendar', function () {
@@ -84,176 +57,72 @@ angular.module('outstanding.date_details', [])
 
 'use strict';
 
-angular.module('app.files.uploader', ['app.files.factory'])
-
-    .directive('fileUploader', ['FilesFactory', function (FilesFactory) {
-        return {
-            restrict: 'A',
-            templateUrl: 'files/files-uploader.html',
-            link: function (scope, element, attrs) {
-                //TODO (S.Panfilov) should refactor with isolated scope and throw errors
-                scope.fileAccept = (attrs.fileAccept) ? attrs.fileAccept : '*/*';
-                scope.fileUploadBtnDesc = (attrs.fileUploadBtnDesc) ? attrs.fileUploadBtnDesc : '';
-                scope.fileUploader = attrs.fileUploader;
-                scope.fileUploadFiledName = attrs.fileUploadFiledName;
-
-                var callbackHandler = scope.$eval(attrs.fileUploaderCallback);
-
-                var handler = function (event) {
-                    var files = event.target.files;
-                    if (files.length === 0) return;
-
-                    FilesFactory.initFilesUpload(scope.fileUploader, files, scope.fileUploadFiledName, callbackHandler);
-                };
-
-                element.bind('change', handler);
-            }
-        };
-    }])
-;
-'use strict';
-
-angular.module('outstanding.files.factory', [])
-
-    .factory('FilesFactory', ['urlMap', 'MessagesFactory', '$timeout', 'HTTP_STATUS', function (urlMap, MessagesFactory, $timeout, HTTP_STATUS) {
-
-        var _messages = {
-            successFileUploadComplete: 'File successful uploaded',
-            errorFileUpload: 'Uploading error',
-            infoUploadCanceled: 'Uploaded cancelled',
-            invalidFileError: 'Unprocessed file'
-        };
-
-        var uploadEvents = {
-            progress: 'progress',
-            load: 'load',
-            error: 'error',
-            abort: 'abort'
-        };
-
-        function prepareFiles(files, fieldName) {
-            var formData = new FormData();
-
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                formData.append(fieldName, file);
-            }
-
-            return formData;
-        }
-
-        var exports = {
-            uploadProgress: 0,
-            files: [],
-            fieldName: 'file',
-            setFiles: function (files) {
-                exports.files = [];
-                for (var i = 0; i < files.length; i++) {
-                    exports.files.push(files[i]);
-                }
-            },
-            clearFiles: function () {
-                exports.files = [];
-            },
-            makeFilesUpload: function (url, callback) {
-                var formData = prepareFiles(exports.files, exports.fieldName);
-
-                var method = 'POST';
-                var xhr = new XMLHttpRequest();
-                xhr.upload.addEventListener(uploadEvents.progress, uploadProgressHandler, false);
-                xhr.addEventListener(uploadEvents.load, uploadCompleteHandler, false);
-                xhr.addEventListener(uploadEvents.error, uploadFailedHandler, false);
-                xhr.addEventListener(uploadEvents.abort, uploadCanceledHandler, false);
-                xhr.open(method, url);
-                xhr.onload = function (event) {
-                    showMessageToUser(event);
-                    exports.cleanUpAfterUpload();
-                    if (callback) callback(event, isError(this.status));
-                };
-                xhr.send(formData);
-            },
-            initFilesUpload: function (url, files, fieldName, callback) {
-                if (fieldName) {
-                    exports.fieldName = fieldName;
-                }
-                exports.setFiles(files);
-                exports.makeFilesUpload(url, callback);
-            },
-            cleanUpAfterUpload: function () {
-                exports.uploadProgress = 0;
-                exports.clearFiles();
-            }
-        };
-
-        function isError(status) {
-            var firstErrorCode = 400;
-            return (status >= firstErrorCode);
-
-        }
-
-        function showMessageToUser(event) {
-            var delay = 1000;
-            var status = event.target.status;
-
-            $timeout(function () {
-                if (status === HTTP_STATUS.UNPROCESSABLE_ENTITY) {
-                    MessagesFactory.errorMsg(_messages.invalidFileError);
-                } else if (isError(status)) {
-                    MessagesFactory.statusErrorMsg(status);
-                } else {
-                    MessagesFactory.successMsg(_messages.successFileUploadComplete);
-                }
-            }, delay);
-
-        }
-
-        function uploadProgressHandler(evt) {
-            if (evt.lengthComputable) {
-                exports.uploadProgress = Math.round(evt.loaded * 100 / evt.total);
-            } else {
-                exports.uploadProgress = '???';
-            }
-        }
-
-        function uploadCompleteHandler(evt) {
-            //var delay = 1000;
-            //$timeout(function () {
-            //    MessagesFactory.successMsg(_messages.successFileUploadComplete);
-            //}, delay);
-        }
-
-        function uploadFailedHandler(evt) {
-            var delay = 1000;
-            $timeout(function () {
-                MessagesFactory.errorMsg(_messages.errorFileUpload);
-            }, delay);
-        }
-
-        function uploadCanceledHandler(evt) {
-            var delay = 1000;
-            $timeout(function () {
-                MessagesFactory.infoMsg(_messages.infoUploadCanceled);
-            }, delay);
-        }
-
-        return exports;
-
-    }])
-;
-
-
-'use strict';
-
 angular.module('outstanding.uploader', [])
 
     .directive('uploader', function () {
         return {
             restrict: 'E',
             replace: true,
-            link: function (scope, elem) {
-                console.log('uploader');
+            templateUrl: 'uploader/uploader.html',
+            scope: {
+                fileReader: "="
+            },
+            link: function (scope, element) {
+
             }
         };
     })
 
+    .directive('fileReader', function () {
+        return {
+            scope: {
+                fileReader: "="
+            },
+            link: function (scope, element) {
+                element.on('change', function (changeEvent) {
+                    var files = changeEvent.target.files;
+                    if (files.length) {
+                        var r = new FileReader();
+                        r.onload = function (e) {
+                            var contents = e.target.result;
+                            scope.$apply(function () {
+                                scope.fileReader = contents;
+                            });
+                        };
+
+                        r.readAsText(files[0]);
+                    }
+                });
+            }
+        };
+    })
+
+
+;
+
+'use strict';
+
+angular.module('outstanding.pages.landing', [
+    'outstanding.calendar',
+    'outstanding.date_details',
+    'outstanding.uploader',
+    'ui.router'
+])
+
+    .config(['$stateProvider', function ($stateProvider) {
+
+        $stateProvider
+            .state('landing', {
+                url: '/landing',
+                templateUrl: 'landing/landing.html',
+                controller: 'LandingPageCtrl'
+            })
+        ;
+    }])
+
+    .controller('LandingPageCtrl', ['$scope', function ($scope) {
+
+        console.log('landing');
+        
+    }])
 ;
--- src/modules/uploader/uploader.js
+++ src/modules/uploader/uploader.js
@@ -6,10 +6,39 @@ angular.module('outstanding.uploader', [])
         return {
             restrict: 'E',
             replace: true,
-            link: function (scope, elem) {
-                console.log('uploader');
+            templateUrl: 'uploader/uploader.html',
+            scope: {
+                fileReader: "="
+            },
+            link: function (scope, element) {
+
             }
         };
     })
+
+    .directive('fileReader', function () {
+        return {
+            scope: {
+                fileReader: "="
+            },
+            link: function (scope, element) {
+                element.on('change', function (changeEvent) {
+                    var files = changeEvent.target.files;
+                    if (files.length) {
+                        var r = new FileReader();
+                        r.onload = function (e) {
+                            var contents = e.target.result;
+                            scope.$apply(function () {
+                                scope.fileReader = contents;
+                            });
+                        };
+
+                        r.readAsText(files[0]);
+                    }
+                });
+            }
+        };
+    })
+
 
 ;
