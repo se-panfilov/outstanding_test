985e71496632be8475be659346f3ae21737190cd
S.Panfilov <se-panfilov@ya.ru>
2015-09-07 13:23:03 +0800

    fixes

--- dist/app.js
+++ dist/app.js
@@ -46,9 +46,72 @@ angular.module('outstanding.calendar', [])
 
     .factory('CalendarFactory', function () {
 
+        function _getDaysInMonth(month, year) {
+            var date = new Date(year, month + 1, 0);
+            if (!exports.isUTC) {
+                return date.getDate();
+            } else {
+                return date.getUTCDate();
+            }
+        }
+
+        function _getDays(monthNum, yearNum) {
+            var result = {};
+            var from = 1;
+            var to = _getDaysInMonth(monthNum, yearNum);
+
+            for (var i = from; i <= to; i++) {
+                result[i] = {};
+            }
+
+            return result;
+        }
+
+        function _getMonthNumber(datetime) {
+            var date = new Date(datetime);
+            if (!exports.isUTC) {
+                return date.getMonth();
+            } else {
+                return date.getUTCMonth();
+            }
+        }
+
+        function getMonth(datetime) {
+            var month = {
+                number: _getMonthNumber(datetime),
+                days: _getDays(datetime)
+            };
+
+            return month;
+        }
+
+        function _getYearNumber(datetime) {
+            var date = new Date(datetime);
+            if (!exports.isUTC) {
+                return date.getFullYear();
+            } else {
+                return date.getUTCFullYear();
+            }
+
+        }
+
+        function _getMonthList(dateTimesList, yearNum) {
+            var result = {};
+            for (var i = 0; i < dateTimesList.length; i++) {
+                var datetime = dateTimesList[i];
+                if (yearNum === _getYearNumber(datetime)) {
+                    var monthNum = _getMonthNumber(datetime);
+                    result[monthNum] = _getDays(monthNum, yearNum);
+                }
+            }
+
+            return result;
+        }
+
+
         var exports = {
             dates: [],
-            months: [],
+            years: [],
             isUTC: false,
             setUTC: function (isUTC) {
                 exports.isUTC = isUTC || false;
@@ -59,15 +122,16 @@ angular.module('outstanding.calendar', [])
                 //make datetime from strings
                 //set export.dates = [123123, 1232132, 3432432]
             },
-            getMonthNumber: function (datetime) {
-                var date = new Date(datetime);
-                if (!exports.isUTC) {
-                    return date.getMonth();
-                } else {
-                    return date.getUTCMonth();
+            makeYearsList: function () {
+                var years = {};
+
+                for (var i = 0; i < exports.dates.length; i++) {
+                    var datetime = exports.dates[i];
+                    var yearNum = _getYearNumber(datetime);
+                    if (!years[yearNum]) { //TODO (S.Panfilov) what if year exist?
+                        years[yearNum] = _getMonthList(exports.dates);
+                    }
                 }
-            },
-            getMonth: function (datetime) {
 
             }
         };
@@ -99,8 +163,10 @@ angular.module('outstanding.calendar', [])
                 function _init(sourceData) {
                     CalendarFactory.setUTC(scope.isUtc);
                     CalendarFactory.setDates(sourceData);
+                    CalendarFactory.makeYearsList();
                 }
 
+                scope.CalendarFactory = CalendarFactory;
 
             }
         };
--- src/modules/calendar/calendar.js
+++ src/modules/calendar/calendar.js
@@ -4,13 +4,22 @@ angular.module('outstanding.calendar', [])
 
     .factory('CalendarFactory', function () {
 
+        function _getDaysInMonth(month, year) {
+            var date = new Date(year, month + 1, 0);
+            if (!exports.isUTC) {
+                return date.getDate();
+            } else {
+                return date.getUTCDate();
+            }
+        }
 
-        function _getDays(datetime) {
-
-            var result = [];
+        function _getDays(monthNum, yearNum) {
+            var result = {};
+            var from = 1;
+            var to = _getDaysInMonth(monthNum, yearNum);
 
             for (var i = from; i <= to; i++) {
-                result.push({num: i});
+                result[i] = {};
             }
 
             return result;
@@ -34,9 +43,33 @@ angular.module('outstanding.calendar', [])
             return month;
         }
 
+        function _getYearNumber(datetime) {
+            var date = new Date(datetime);
+            if (!exports.isUTC) {
+                return date.getFullYear();
+            } else {
+                return date.getUTCFullYear();
+            }
+
+        }
+
+        function _getMonthList(dateTimesList, yearNum) {
+            var result = {};
+            for (var i = 0; i < dateTimesList.length; i++) {
+                var datetime = dateTimesList[i];
+                if (yearNum === _getYearNumber(datetime)) {
+                    var monthNum = _getMonthNumber(datetime);
+                    result[monthNum] = _getDays(monthNum, yearNum);
+                }
+            }
+
+            return result;
+        }
+
+
         var exports = {
             dates: [],
-            months: [],
+            years: [],
             isUTC: false,
             setUTC: function (isUTC) {
                 exports.isUTC = isUTC || false;
@@ -47,7 +80,16 @@ angular.module('outstanding.calendar', [])
                 //make datetime from strings
                 //set export.dates = [123123, 1232132, 3432432]
             },
-            makeMonthsList: function () {
+            makeYearsList: function () {
+                var years = {};
+
+                for (var i = 0; i < exports.dates.length; i++) {
+                    var datetime = exports.dates[i];
+                    var yearNum = _getYearNumber(datetime);
+                    if (!years[yearNum]) { //TODO (S.Panfilov) what if year exist?
+                        years[yearNum] = _getMonthList(exports.dates);
+                    }
+                }
 
             }
         };
@@ -79,8 +121,10 @@ angular.module('outstanding.calendar', [])
                 function _init(sourceData) {
                     CalendarFactory.setUTC(scope.isUtc);
                     CalendarFactory.setDates(sourceData);
+                    CalendarFactory.makeYearsList();
                 }
 
+                scope.CalendarFactory = CalendarFactory;
 
             }
         };
